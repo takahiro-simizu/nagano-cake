3cf4e3c6d41eab88209ad91e12cb49c254b80b04
takahiro-shimizu <tkbchando@gmail.com>
2021-05-29 10:59:06 +0000

    [Add]各種コントローラーの追加

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,18 @@
+class Public::CustomersController < ApplicationController
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def resign
+  end
+
+  def resign_prcs
+  end
+
+end
---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,21 @@
+class Public::OrdersController < ApplicationController
+
+  def new
+  end
+
+  def check
+  end
+
+  def thanks
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+end
