85ccd9aeaabaa1951fa3b829a45112077cab20d1
takahiro-shimizu <tkbchando@gmail.com>
2021-06-11 08:36:36 +0000

    [Add]会員情報編集機能(管理者)の追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,12 +5,25 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer)
+          .permit(:customer_id, :last_name, :first_name, :kana_last_name, :kana_first_name,
+            :postal_code, :address, :phone_number, :email, :is_active)
   end
 
 end
